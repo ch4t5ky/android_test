01ca16efe15d5bce229ebb90af2dfb88a6818b29
ch4t5ky <mr_a953@mail.ru>
2022-07-15 02:38:48 +0300

    Add scrollView, install button and xml for review

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.sirius.test_app
 
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private val INTERNET_PERMISSION_CODE = 100
+    var dataModel = DataModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,19 +29,33 @@ class MainActivity : AppCompatActivity() {
         val reviewCountText: TextView = this.findViewById(R.id.reviewsCountText)
         val gradeCnt: TextView = this.findViewById(R.id.gradeCntView)
         val ratingBar: RatingBar = this.findViewById(R.id.ratingBar)
+        val ratingBar2: RatingBar = this.findViewById(R.id.ratingBar2)
         val banner: ImageView = this.findViewById(R.id.banner)
         val logo: ImageView = this.findViewById(R.id.logo)
 
-        var dataModel = DataModel()
+
+
         nameView.text = dataModel.name
         descriptionView.text = dataModel.description
         ratingText.text = dataModel.rating.toString()
         reviewCountText.text = "${dataModel.gradeCnt} Reviews"
         gradeCnt.text = dataModel.gradeCnt
         ratingBar.rating = dataModel.rating
+        ratingBar2.rating = dataModel.rating
+
+
+        val lparams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        for (element in dataModel.tags){
+            val textView: TextView = TextView(this);
+            textView.text = element
+            textView.layoutParams = lparams
+            textView.setTextColor(Color.parseColor("#44A9F4"))
+        }
+
         Picasso.with(this).load(dataModel.image).into(banner)
         Picasso.with(this).load(dataModel.logo).into(logo)
     }
+
 
     private fun checkPermission(permission: String, requestCode: Int) {
         if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
